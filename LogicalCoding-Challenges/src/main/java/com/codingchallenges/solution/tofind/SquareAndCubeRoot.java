39d407322052449e391f9c171a0882795731d322
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-01-04 13:28:52 +0530

    Computing the square root and cube root of an integer using Core Java & Java8

---
+++ LogicalCoding-Challenges/src/main/java/com/codingchallenges/solution/tofind/SquareAndCubeRoot.java
@@ -0,0 +1,15 @@
+package com.codingchallenges.solution.tofind;
+
+public class SquareAndCubeRoot {
+
+	public static void main(String[] args) {
+
+		int num = 16;
+		
+		double squareRoot = Math.sqrt(num);
+		double cubeRoot = Math.cbrt(num);
+
+		System.out.println("Square root: " + squareRoot);
+		System.out.println("Cube root: " + cubeRoot);
+	}
+}
---
+++ LogicalCoding-Challenges/src/main/java/com/codingchallenges/solution/tofind/SquareAndCubeRootJava8.java
@@ -0,0 +1,21 @@
+package com.codingchallenges.solution.tofind;
+
+import java.util.Arrays;
+
+public class SquareAndCubeRootJava8 {
+
+    public static void main(String[] args) {
+
+        int num = 81;
+
+        // Use Java 8 Streams to calculate the square root and cube root
+        Arrays.asList(num)  // Wrap the number in a list to use streams (if needed for more numbers)
+            .forEach(n -> {
+                double squareRoot = Math.sqrt(n);
+                double cubeRoot = Math.cbrt(n);
+
+                System.out.println("Square root of " + n + ": " + squareRoot);
+                System.out.println("Cube root of " + n + ": " + cubeRoot);
+            });
+    }
+}

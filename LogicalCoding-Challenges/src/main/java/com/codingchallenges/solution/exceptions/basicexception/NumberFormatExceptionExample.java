88153410bdae81a4dd2e8fb0b56aeafde1aa9202
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-01-05 13:56:59 +0530

    NumberFormatException Example Use try-catch-finally to handle it.

---
+++ LogicalCoding-Challenges/src/main/java/com/codingchallenges/solution/exceptions/basicexception/NumberFormatExceptionExample.java
@@ -0,0 +1,17 @@
+// Write a program to demonstrate NumberFormatException in Java. & Use try-catch-finally to handle it.
+
+package com.codingchallenges.solution.exceptions.basicexception;
+
+public class NumberFormatExceptionExample {
+	
+    public static void main(String[] args) {
+    	
+        try {
+            int num = Integer.parseInt("ABC"); // Invalid conversion
+        } catch (NumberFormatException e) {
+            System.out.println("Caught NumberFormatException: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block executed.");
+        }
+    }
+}

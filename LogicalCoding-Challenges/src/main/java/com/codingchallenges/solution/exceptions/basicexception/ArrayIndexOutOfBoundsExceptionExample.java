149fcd6b638bf4956672f450b1ca2c66f316bf8c
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-01-05 13:38:09 +0530

    ArrayIndexOutOfBoundsException eg. using try-catch to handle Exception

---
+++ LogicalCoding-Challenges/src/main/java/com/codingchallenges/solution/exceptions/basicexception/ArrayIndexOutOfBoundsExceptionExample.java
@@ -0,0 +1,17 @@
+// Write a program to demonstrate ArrayIndexOutOfBoundsException in Java.
+
+package com.codingchallenges.solution.exceptions.basicexception;
+
+public class ArrayIndexOutOfBoundsExceptionExample {
+
+	public static void main(String[] args) {
+
+		try {
+			int[] arr = { 1, 2, 3 };
+			System.out.println(arr[5]); // Accessing an index out of bounds
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
+		}
+	}
+
+}
